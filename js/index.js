e9e6f3c42fe6e23c8fb08e29320da82dfceec8b7
wangqiuwei <wqw_0815@163.com>
2019-11-30 21:53:45 +0800

    learn alert
     On branch master
     Changes to be committed:
    	modified:   css/style.css
    	modified:   index.html
    	new file:   index3.html
    	modified:   js/index.js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+    $('.close-alert').click(()=> {
+        $('.alert').alert('close')
+    })
+
+    $('.alert').on('close.bs.alert',()=>{
+        console.log('close alert.');
+    })
+
+    $('.alert').on('closed.bs.alert',()=>{
+        console.log('closed alert.');
+    })
+})
